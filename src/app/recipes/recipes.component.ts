45048d329cb795caef2b00131fd900cdebc67157
Cheyenne Barton <cheyenneb5512@gmail.com>
2023-01-18 18:36:21 -0500

    Made dropdown menus

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -7,7 +7,7 @@ import { Recipe } from './recipe.model';
   styleUrls: ['./recipes.component.scss'],
 })
 export class RecipesComponent implements OnInit {
-  selectedRecipe: Recipe;
+ selectedRecipe: Recipe;
 
   constructor() {}
 
