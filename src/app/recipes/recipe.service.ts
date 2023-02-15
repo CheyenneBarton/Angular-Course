e1143cd1517475e2441248cf65ccd1ab037ee7ba
Cheyenne Barton <cheyenneb5512@gmail.com>
2023-02-15 18:10:26 -0500

    Added service.ts

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,20 @@
+import { Recipe } from "./recipe.model";
+
+export class RecipeService {
+    private recipes: Recipe[] = [
+        new Recipe(
+          'A Test Recipe',
+          'This is the description',
+          'https://static01.nyt.com/images/2022/03/02/dining/kc-pasta-amatriciana/merlin_201502869_17a64c2f-adee-4526-9951-a0b03270b022-superJumbo.jpg?quality=75&auto=webp'
+        ),
+        new Recipe(
+          'A Second Test Recipe',
+          'This is another description',
+          'https://static01.nyt.com/images/2022/03/02/dining/kc-pasta-amatriciana/merlin_201502869_17a64c2f-adee-4526-9951-a0b03270b022-superJumbo.jpg?quality=75&auto=webp'
+        ),
+      ];
+
+      getRecipes(){
+        return this.recipes.slice();
+      }
+}
