b98f1cbde96b83eb8c49a4c66344ad4e0131a696
Cheyenne Barton <cheyenneb5512@gmail.com>
2023-02-17 13:56:39 -0500

    Observables

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,8 @@
-import { EventEmitter } from "@angular/core";
+import {Subject} from 'rxjs';
 import { Ingredient } from "../shared/ingredient.model";
 
 export class ShoppngListService {
-ingredientsChanged = new EventEmitter<Ingredient[]>();
+ingredientsChanged = new Subject<Ingredient[]>();
 
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
@@ -15,15 +15,14 @@ getIngredients () {
 
 addIngredient(ingredient: Ingredient) {
 this.ingredients.push(ingredient);
-this.ingredientsChanged.emit(this.ingredients.slice());
+this.ingredientsChanged.next(this.ingredients.slice());
 }
 
 addIngredients(ingredients: Ingredient[]){
 // for (let ingredient of ingredients) {
 //     this.addIngredient(ingredient);
-// }
 
 this.ingredients.push(...ingredients); //spread operator 
-this.ingredientsChanged.emit(this.ingredients.slice())
+this.ingredientsChanged.next(this.ingredients.slice())
 }
 }
